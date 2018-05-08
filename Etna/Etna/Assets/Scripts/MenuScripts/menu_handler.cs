3d51539d1e26dbf266419ec187577aa8e916c766
Tiemen Kamp <t.kamp@sintlucasedu.nl>
2018-05-08 10:42:26 +0200

    Starting build

---
+++ Etna/Etna/Assets/Scripts/MenuScripts/menu_handler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu_handler : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Loaded scene");
+        SceneManager.LoadScene("LevelOne", LoadSceneMode.Single);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
