d7983f551d369bcaee116bfdec07a9bc1b8946f3
Pieter Schreurs <pieter_j_schreurs@hotmail.com>
2018-05-08 09:57:51 +0200

    torchlight prefab, main menu

---
+++ Etna/Etna/Assets/ActivationLight.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationLight : MonoBehaviour
+{
+
+    public Light light;
+    private bool _isActivated;
+    public float maxLightIntensity;
+    public float maxLightRange;
+    public float TimeTillExtinguish;
+    private float timeLeft = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        _isActivated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isActivated)
+        {
+            timeLeft -= Time.deltaTime;
+            if(timeLeft <= 0)
+            {
+                TurnOffLight();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ActivateLight();
+        }
+    }
+
+    public void ActivateLight()
+    {
+        _isActivated = true;
+        light.intensity = maxLightIntensity;
+        timeLeft = TimeTillExtinguish;
+    }
+
+    public void TurnOffLight()
+    {
+        _isActivated = false;
+        light.intensity = 0;
+    }
+}
