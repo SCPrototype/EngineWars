065632d572222e24f1f8ac5191cb3abe03a72adc
Pieter Schreurs <pieter_j_schreurs@hotmail.com>
2018-05-09 11:52:11 +0200

    Wallrunning, plus demo level.

--- Etna/Etna/Assets/Scripts/PlayerMovement.cs
+++ Etna/Etna/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody), typeof(ParticleSystem))]
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     public float MovementSpeed;
     public float MovementSpeedInAir;
@@ -12,24 +13,35 @@ public class PlayerMovement : MonoBehaviour {
     public float MaxSpeedInAir;
     public float JumpHeight;
     private bool isGrounded;
+    private bool isOnWall;
     private Rigidbody rb;
 
     private const float FOVChange = 25;
     private float baseFOV;
     private ParticleSystem dynamicLines;
     private BoxCollider feetCollider;
+    private BoxCollider rightCollider;
+    private BoxCollider leftCollider;
+    private GameObject rightBox;
+    private GameObject leftBox;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rb = GetComponent<Rigidbody>();
         dynamicLines = GetComponent<ParticleSystem>();
         baseFOV = Camera.main.fieldOfView;
         feetCollider = GetComponent<BoxCollider>();
+        rightBox = GameObject.Find("SideColliderRight");
+        rightCollider = rightBox.GetComponent<BoxCollider>();
+        leftBox = GameObject.Find("SideColliderLeft");
+        leftCollider = leftBox.GetComponent<BoxCollider>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         HandleInput();
         HandleGrounded();
         HandleWall();
@@ -40,25 +52,60 @@ public class PlayerMovement : MonoBehaviour {
         isGrounded = false;
         Collider[] allOverlappingColliders = Physics.OverlapBox(feetCollider.bounds.center, feetCollider.bounds.extents);
 
-        foreach(Collider collidedObject in allOverlappingColliders)
+        foreach (Collider collidedObject in allOverlappingColliders)
         {
-            if(collidedObject.tag == "Ground")
+            if (collidedObject.tag == "Ground")
             {
                 isGrounded = true;
                 break;
             }
         }
+
+        
     }
 
     private void HandleWall()
     {
-        
+        isOnWall = false;
+        Collider[] allOverlappingCollidersLeft = Physics.OverlapBox(rightCollider.bounds.center, rightCollider.bounds.extents);
+
+        foreach (Collider collidedObject in allOverlappingCollidersLeft)
+        {
+            if (collidedObject.tag == "Wall")
+            {
+                Debug.Log("Character is on a wall left");
+                isOnWall = true;
+                break;
+            }
+        }
+
+        Collider[] allOverlappingCollidersRight = Physics.OverlapBox(leftCollider.bounds.center, leftCollider.bounds.extents);
+
+        foreach (Collider collidedObject in allOverlappingCollidersRight)
+        {
+            if (collidedObject.tag == "Wall")
+            {
+                Debug.Log("Character is on a wall right");
+                isOnWall = true;
+                break;
+            }
+        }
     }
 
     private void HandleInput()
     {
+        if (isOnWall)
+        {
+            rb.useGravity = false;
+            isGrounded = true;
+        }
+        else
+        {
+            rb.useGravity = true;
+        }
         if (isGrounded)
         {
+
             rb.drag = 3;
             if (Input.GetKey(KeyCode.W))
             {
@@ -82,9 +129,10 @@ public class PlayerMovement : MonoBehaviour {
             }
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                rb.AddForce(transform.up * JumpHeight);  
+                rb.AddForce(transform.up * JumpHeight);
             }
-        } else
+        }
+        else
         {
             rb.drag = 1;
             if (Input.GetKey(KeyCode.W))
@@ -108,7 +156,7 @@ public class PlayerMovement : MonoBehaviour {
                 rb.velocity = rb.velocity.normalized * MaxSpeedInAir;
             }
         }
-        
+
         //if (rb.velocity.magnitude >= 0.3f)
         //{
         //    Camera.main.fieldOfView = baseFOV + (FOVChange * (rb.velocity.magnitude / MaxSpeed));
