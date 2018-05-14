0c698a2caf65a859884e8ff5176b019e97540486
Tiemen Kamp <t.kamp@sintlucasedu.nl>
2018-05-14 10:36:41 +0200

    Implemented a base for interacting. Implemented basic Vaulting and Sliding. Cleaned up LevelTwo scene a bit.

---
+++ Etna/Etna/Assets/Scripts/Interactable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+    public KeyCode interactKey;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public virtual void Interact(PlayerMovement target)
+    {
+        Debug.Log("Empty interactable.");
+    }
+
+    public virtual KeyCode GetInteractKey()
+    {
+        return interactKey;
+    }
+
+    public float Dot(Vector2 origin, Vector2 target)
+    {
+        Vector2 normalizedTarget = target.normalized;
+        float dot = origin.x * target.x + origin.y * target.y;
+        return dot;
+    }
+
+    public float GetAngleDifference(Vector2 origin, Vector2 target)
+    {
+        float dotProduct = Dot(origin, target);
+        float angleDiff = dotProduct / (origin.magnitude * target.magnitude);
+        return angleDiff;
+    }
+}
--- Etna/Etna/Assets/Scripts/PlayerMovement.cs
+++ Etna/Etna/Assets/Scripts/PlayerMovement.cs
@@ -25,6 +25,26 @@ public class PlayerMovement : MonoBehaviour
     private GameObject rightBox;
     private GameObject leftBox;
 
+    private BoxCollider interactCollider;
+    private Vector3 velocityHolder;
+    private Vector3 vaultStart;
+    private Vector3 vaultTarget;
+
+    private enum MovementState
+    {
+        Idle,
+        Walk,
+        Run,
+        Jump,
+        Fall,
+        Vault,
+        Slide,
+        WallRun,
+        WallCling
+    }
+
+    private MovementState myMovementState = MovementState.Idle;
+
 
     // Use this for initialization
     void Start()
@@ -32,11 +52,10 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         dynamicLines = GetComponent<ParticleSystem>();
         baseFOV = Camera.main.fieldOfView;
-        feetCollider = GetComponent<BoxCollider>();
-        rightBox = GameObject.Find("SideColliderRight");
-        rightCollider = rightBox.GetComponent<BoxCollider>();
-        leftBox = GameObject.Find("SideColliderLeft");
-        leftCollider = leftBox.GetComponent<BoxCollider>();
+        feetCollider = GameObject.Find("FeetCollider").GetComponent<BoxCollider>();
+        rightCollider = GameObject.Find("SideColliderRight").GetComponent<BoxCollider>();
+        leftCollider = GameObject.Find("SideColliderLeft").GetComponent<BoxCollider>();
+        interactCollider = GameObject.Find("InteractCollider").GetComponent<BoxCollider>(); ;
     }
 
     // Update is called once per frame
@@ -44,18 +63,19 @@ public class PlayerMovement : MonoBehaviour
     {
         if (!GameMenu_Handler.Paused)
         {
-            if (rb.isKinematic)
-            {
-                rb.isKinematic = false;
-            }
+            //if (rb.isKinematic)
+            //{
+            //    rb.isKinematic = false;
+            //}
             HandleInput();
             HandleGrounded();
             HandleWall();
+            HandleState();
         }
-        else if (!rb.isKinematic)
-        {
-            rb.isKinematic = true;
-        }
+        //else if (!rb.isKinematic)
+        //{
+        //    rb.isKinematic = true;
+        //}
     }
 
     private void HandleGrounded()
@@ -116,56 +136,52 @@ public class PlayerMovement : MonoBehaviour
         }
         if (isGrounded)
         {
-
-            rb.drag = 3;
-            if (Input.GetKey(KeyCode.W))
+            if (myMovementState != MovementState.Vault && myMovementState != MovementState.Slide)
             {
-                rb.AddForce(transform.forward * MovementSpeed * Time.deltaTime);
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                rb.AddForce(-transform.right * MovementSpeed * Time.deltaTime);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                rb.AddForce(-transform.forward * MovementSpeed * Time.deltaTime);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                rb.AddForce(transform.right * MovementSpeed * Time.deltaTime);
-            }
-            if (rb.velocity.magnitude >= MaxSpeed)
-            {
-                rb.velocity = rb.velocity.normalized * MaxSpeed;
-            }
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                rb.AddForce(transform.up * JumpHeight);
+                rb.drag = 3;
+                if (Input.GetKey(KeyCode.W))
+                {
+                    rb.velocity += transform.forward * MovementSpeed * Time.deltaTime;
+                    //rb.AddForce(transform.forward * MovementSpeed * Time.deltaTime);
+                }
+                if (Input.GetKey(KeyCode.A))
+                {
+                    //rb.AddForce(-transform.right * MovementSpeed * Time.deltaTime);
+                    rb.velocity += -transform.right * MovementSpeed * Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    //rb.AddForce(-transform.forward * MovementSpeed * Time.deltaTime);
+                    rb.velocity += -transform.forward * MovementSpeed * Time.deltaTime;
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    //rb.AddForce(transform.right * MovementSpeed * Time.deltaTime);
+                    rb.velocity += transform.right * MovementSpeed * Time.deltaTime;
+                }
+                if (new Vector3(rb.velocity.x, 0, rb.velocity.z).magnitude >= MaxSpeed)
+                {
+                    rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z).normalized * MaxSpeed + new Vector3(0, rb.velocity.y, 0);
+                }
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    if (!checkInteractable(KeyCode.Space))
+                    {
+                        Jump();
+                    }
+                }
+                if (Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                    if (!checkInteractable(KeyCode.LeftShift))
+                    {
+                        //Jump();
+                    }
+                }
             }
         }
         else
         {
-            rb.drag = 1;
-            if (Input.GetKey(KeyCode.W))
-            {
-                rb.AddForce(transform.forward * MovementSpeedInAir * Time.deltaTime);
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                rb.AddForce(-transform.right * MovementSpeedInAir * Time.deltaTime);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                rb.AddForce(-transform.forward * MovementSpeedInAir * Time.deltaTime);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                rb.AddForce(transform.right * MovementSpeedInAir * Time.deltaTime);
-            }
-            if (rb.velocity.magnitude >= MaxSpeed)
-            {
-                rb.velocity = rb.velocity.normalized * MaxSpeedInAir;
-            }
+            rb.drag = 0.1f;
         }
 
         //if (rb.velocity.magnitude >= 0.3f)
@@ -178,5 +194,114 @@ public class PlayerMovement : MonoBehaviour
         //    dynamicLines.Stop();
         //    Camera.main.fieldOfView = baseFOV;
         //}
+    }
+
+    private void HandleState()
+    {
+        switch (myMovementState)
+        {
+            case MovementState.Idle:
+                break;
+            case MovementState.Walk:
+                break;
+            case MovementState.Run:
+                break;
+            case MovementState.Jump:
+                break;
+            case MovementState.Fall:
+                break;
+            case MovementState.Vault:
+                Debug.Log("vaulting");
+                transform.position += (vaultTarget - vaultStart) * Time.deltaTime * (0.5f + (velocityHolder.magnitude / 4));
+                if (Vector3.Distance(transform.position, vaultStart) >= Vector3.Distance(vaultTarget, vaultStart))
+                {
+                    rb.isKinematic = false;
+                    rb.velocity = velocityHolder;
+                    transform.position += new Vector3(0, -1, 0);
+                    if (velocityHolder.magnitude > 0)
+                    {
+                        myMovementState = MovementState.Run;
+                    }
+                    else
+                    {
+                        myMovementState = MovementState.Idle;
+                    }
+                }
+                break;
+            case MovementState.Slide:
+                Debug.Log("sliding");
+                transform.position += (vaultTarget - vaultStart) * Time.deltaTime * (0.5f + (velocityHolder.magnitude / 4));
+                if (Vector3.Distance(transform.position, vaultStart) >= Vector3.Distance(vaultTarget, vaultStart))
+                {
+                    rb.isKinematic = false;
+                    rb.velocity = velocityHolder;
+                    transform.position += new Vector3(0, 1.5f, 0);
+                    if (velocityHolder.magnitude > 0)
+                    {
+                        myMovementState = MovementState.Run;
+                    }
+                    else
+                    {
+                        myMovementState = MovementState.Idle;
+                    }
+                }
+                break;
+            case MovementState.WallRun:
+                break;
+            case MovementState.WallCling:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private bool checkInteractable(KeyCode key)
+    {
+        Collider[] allOverlappingColliders = Physics.OverlapBox(interactCollider.bounds.center, interactCollider.bounds.extents);
+
+        foreach (Collider collidedObject in allOverlappingColliders)
+        {
+            if (collidedObject.GetComponent<Interactable>() != null)
+            {
+                if (collidedObject.GetComponent<Interactable>().GetInteractKey() == key)
+                {
+                    collidedObject.GetComponent<Interactable>().Interact(this);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public Rigidbody GetRigidBody()
+    {
+        return rb;
+    }
+
+    public void Vault(Vector3 start, Vector3 end)
+    {
+        velocityHolder = rb.velocity;
+        rb.isKinematic = true;
+        transform.position += new Vector3(0, 1, 0);
+        vaultStart = transform.position;
+        vaultTarget = transform.position + ((end - start).normalized * Vector3.Distance(transform.position, end));
+        //transform.position += end - start;
+        myMovementState = MovementState.Vault;
+    }
+
+    public void Slide(Vector3 start, Vector3 end)
+    {
+        velocityHolder = rb.velocity;
+        rb.isKinematic = true;
+        transform.position += new Vector3(0, -1.5f, 0);
+        vaultStart = transform.position;
+        vaultTarget = transform.position + ((end - start).normalized * Vector3.Distance(transform.position, end));
+        myMovementState = MovementState.Slide;
+    }
+
+    public void Jump()
+    {
+        myMovementState = MovementState.Jump;
+        rb.AddForce(transform.up * JumpHeight);
     }
 }
